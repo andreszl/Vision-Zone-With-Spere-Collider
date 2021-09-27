deef2f23c721289ad57de9242c7ed83f02ad81db
andreszl <andreszl.devcode@gmail.com>
2021-09-27 06:44:26 -0500

    initial commit -n

---
+++ Assets/VisionZone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class VisionZone : MonoBehaviour {
+    public List<Collider> enemies;
+
+    void OnTriggerEnter(Collider other) {
+        print("new enemy " + other.name);
+        enemies.Add(other);
+    }
+
+     void OnTriggerExit(Collider other) {
+        print("leave enemy " + other.name);
+        enemies.Remove(other);
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.DrawWireSphere(transform.position, 1f);
+    } 
+}
